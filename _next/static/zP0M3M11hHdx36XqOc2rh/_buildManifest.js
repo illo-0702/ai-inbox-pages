252338ac2ba64c98e7f1c9bd18c5d9f03c49d520
illo <lhaa0130@gmail.com>
2026-09-19 10:52:04 +0900

    Deploy static site to GitHub Pages

---
+++ _next/static/zP0M3M11hHdx36XqOc2rh/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/ai-inbox-pages//_next/:path+",
+        "destination": "/ai-inbox-pages/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
